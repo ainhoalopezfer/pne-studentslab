85f5ece9dc9d5dc0af8e4789ef92d794918b4f97
ainhoalopezfer <a.lopezfer.2023@alumnos.urjc.es>
2024-02-06 16:42:18 +0100

    S04

--- S04/body.py
+++ S04/body.py
@@ -3,7 +3,7 @@ FILENAME = "Sequences/U5.txt"
 file_contents = Path(FILENAME).read_text()
 
 lines = file_contents.split("\n")
-print("Body of the U5.txt file:")
-for i in range(1, len(lines)):
-    print(lines[i])
+lines.pop(0)
+print("Body of the U5.txt file:\n" + str("\n".join(lines)))
+
 

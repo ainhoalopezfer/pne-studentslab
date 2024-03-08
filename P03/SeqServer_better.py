75390a65be94c08a76d233e59c448235ae237ae8
ainhoalopezfer <a.lopezfer.2023@alumnos.urjc.es>
2024-03-08 14:11:36 +0100

    S04

--- P03/SeqServer_better.py
+++ P03/SeqServer_better.py
@@ -1,24 +1,17 @@
 import socket
 from client1 import Client
 from Seq2 import Seq
-
-
 def ping(client):
     client.ping()
     return "OK!\n"
-
-
 def get(client, n):
     seq = ["ATGGGCTTA", "TTAGGCTA", "TAGGTTCGA", "GCCTAGCGAT", "GATTCGATC"]
     client.get()
     return seq[n] + "\n"
-
-
 def info(client, seq_str):
     s = Seq(seq_str)
     client.info()
     return f"Sequence {seq_str}\nTotal Length: {s.len()}\nA: {s.count('A')}\nC: {s.count('C')}\nG: {s.count('G')}\nT: {s.count('T')}\n"
-
 def comp(client, seq_str):
     s = Seq(seq_str)
     client.comp()
@@ -34,46 +27,41 @@ def read_fasta(client, file):
     return s.read_fasta("../Sequences/" + file + ".txt") + "\n"
 
 
-# Define other command handlers...
+PORT = 8081
+IP = "127.0.0.1"
 
-def main():
-    PORT = 8081
-    IP = "127.0.0.1"
+c = Client(IP, PORT)
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.bind((IP, PORT))
+ls.listen()
 
-    c = Client(IP, PORT)
-    ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ls.bind((IP, PORT))
-    ls.listen()
+print("SEQ server configured!")
 
-    print("SEQ server configured!")
+while True:
+    print("Waiting for clients")
+    (rs, address) = ls.accept()
+    msg = rs.recv(2048).decode("utf-8")
 
-    while True:
-        print("Waiting for clients")
-        (rs, address) = ls.accept()
-        msg = rs.recv(2048).decode("utf-8")
+    command, *args = msg.split()
+    if command == "PING":
+        nmsg = ping(c)
+    elif command == "GET":
+        nmsg = get(c, int(args[0]))
+    elif command == "INFO":
+        nmsg = info(c, args[0])
+    elif command == "COMP":
+        nmsg = comp(c, args[0])
+    elif command == "REV":
+        nmsg = rev(c, args[0])
+    elif command == "GENE":
+        nmsg = read_fasta(c, args[0])
+    # Add other commands...
+    else:
+        nmsg = "Invalid command\n"
 
-        command, *args = msg.split()
-        if command == "PING":
-            nmsg = ping(c)
-        elif command == "GET":
-            nmsg = get(c, int(args[0]))
-        elif command == "INFO":
-            nmsg = info(c, args[0])
-        elif command == "COMP":
-            nmsg = comp(c, args[0])
-        elif command == "REV":
-            nmsg = rev(c, args[0])
-        elif command == "GENE":
-            nmsg = read_fasta(c, args[0])
-        # Add other commands...
-        else:
-            nmsg = "Invalid command\n"
+    print(nmsg)
+    rs.send(nmsg.encode())
 
-        print(nmsg)
-        rs.send(nmsg.encode())
+    rs.close()
 
-        rs.close()
-
-    ls.close()
-if __name__ == "__main__":
-    main()
+ls.close()
